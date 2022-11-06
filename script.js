e55f858c0a029f4c4935e332ffe96c4044041dba
Mysiogonek <madeleine509@gmail.com>
2022-11-06 12:02:56 +0100

    Liczarka works

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
-// osoby = document.querySelector("#imie");
+
 usersDiv = document.querySelector("#users");
-// paragraf = document.querySelector("#abc");
-// let nowyDiv;
-// let noweP;
 
-// const wczesniejszePunkty = document.querySelector("#suma");
-// let przycisk = document.querySelector(".btn-add");
-// let input = document.querySelector("input");
-// const wpisane = parseInt(input.value);
-
-const dodawanie = () => {
-  if (input.value !== "") {
-    console.log("input.value", input.value);
-
-    let a = parseInt(input.value);
-    input.value = "";
-    let b = parseInt(wczesniejszePunkty.textContent) || 0;
-
-    console.log({ a, b });
-
-    wczesniejszePunkty.textContent = a + b;
-  }
-};
-
-// przycisk.addEventListener("click", dodawanie);
-// input.addEventListener("keydown", function (event) {
-//   if (event.code == "Enter") {
-//     dodawanie();
-//   }
-// });
 
 const createNewUser = (userName) => {
   let newUserDiv = document.createElement("div");
   newUserDiv.innerHTML = `
-  <div>${userName}</div>
-  <span id="suma">0</span>
+  <div>${userName}: <span id="suma">0</span></div>
+  
   <input type="number" class="todo-input" placeholder="Wpisz punkty">
   <button class="btn-add">Dodaj</button>
   `;
@@ -44,35 +16,39 @@ const createNewUser = (userName) => {
   const input = newUserDiv.querySelector("input");
   const points = newUserDiv.querySelector("#suma");
 
-  addButton.addEventListener("click", () => {
-    alert(`Kliknęliśmy guzik użytkownika ${userName}`);
+  const addPoints = () => {
+    if (input.value !== "") {
+      console.log("input.value", input.value);
+  
+      let a = parseInt(input.value);
+      input.value = "";
+      let b = parseInt(points.textContent) || 0;
+  
+      console.log({ a, b });
+  
+      points.textContent = a + b;
+    }
+  };
+
+  
+   addButton.addEventListener("click", addPoints);
+ input.addEventListener("keydown", function (event) {
+  if (event.code == "Enter") {
+   addPoints();
+   }
   });
 
-  // const wczesniejszePunkty2 = document.querySelector("#suma");
-
-  // let noweP = document.createElement("p");
-  // noweP.innerHTML = userName;
-  // newUserDiv.appendChild(noweP);
-  // noweP.innerHTML = "";
-
-  // let nowySpan = document.createElement("span");
-  // nowySpan.innerHTML = " ";
-  // noweP.appendChild(nowySpan);
-
-  // let input2 = document.querySelector(".todo-input");
-
-  // let button2 = document.querySelector(".btn-add");
+ 
 };
 
-// let person;
-// const userNames = [];
+
 
 let newUserName;
 do {
   newUserName = prompt("Jak masz na imię?");
-  if (newUserName.length < 1) {
+  if (newUserName == null || newUserName.length < 1) {
     break;
   }
-  // userNames.push(newUserName);
+  
   createNewUser(newUserName);
 } while (true);
